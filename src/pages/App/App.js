0244eca3acbdd3701c5a64215324d7a0231a58e5
aliravn <alira.vn@gmail.com>
2022-11-12 20:31:40 +0100

    initialize app setup

---
+++ src/pages/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+	return (
+		<div className={styles.app}>
+			<header className={styles.header}> I am the APP</header>
+		</div>
+	);
+}
+
+export default App;
