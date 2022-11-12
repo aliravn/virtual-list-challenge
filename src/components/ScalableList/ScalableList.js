50eac4d617c5ca3e82258fdd49d9401446786ded
aliravn <alira.vn@gmail.com>
2022-11-12 23:48:43 +0100

    create first version of scalable list

---
+++ src/components/ScalableList/ScalableList.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from 'components/ScalableList/ScalableList.module.scss';
+
+const ScalableList = props => {
+	const { itemsTotal, itemHeight, renderItem, listHeight } = props;
+	const [scrollTop, setScrollTop] = useState(0);
+
+	const innerHeight = itemsTotal * itemHeight;
+	const startIndex = Math.floor(scrollTop / itemHeight);
+	const endIndex = Math.ceil((scrollTop + listHeight) / itemHeight - 1);
+
+	const items = [];
+	for (let i = startIndex; i <= endIndex; i++) {
+		items.push(
+			renderItem({
+				index: i,
+				style: {
+					position: 'absolute',
+					top: `${i * itemHeight}px`,
+					width: '100%',
+				},
+			})
+		);
+	}
+
+	const handleScroll = e => setScrollTop(e.currentTarget.scrollTop);
+
+	return (
+		<div className={styles.visibleArea} onScroll={handleScroll}>
+			<ul style={{ position: 'relative', height: `${innerHeight}px` }}>{items}</ul>
+		</div>
+	);
+};
+
+export default ScalableList;
