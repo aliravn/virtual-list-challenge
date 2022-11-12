894b3906fd981a25c514525ea20e040b3f9832d7
aliravn <alira.vn@gmail.com>
2022-11-12 20:46:22 +0100

    render a short list of Tickets

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Ticket from 'components/Ticket/Ticket.js';
 import styles from './App.module.scss';
+
+const tickets = [...Array(10).keys()].map(i => {
+	return { id: i };
+});
 
 function App() {
 	return (
 		<div className={styles.app}>
-			<header className={styles.header}> I am the APP</header>
+			{tickets.map(item => (
+				<Ticket />
+			))}
 		</div>
 	);
 }
