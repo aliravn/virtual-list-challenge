7999ec00797f962ed471bc7cc2c28ac1f319e184
aliravn <alira.vn@gmail.com>
2022-11-12 20:45:54 +0100

    create basic Ticket component

---
+++ src/components/Ticket/Ticket.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from 'components/Ticket/Ticket.module.scss';
+
+const Ticket = () => {
+	return <div className={styles.wrapper}>I am a Ticket Component</div>;
+};
+
+export default Ticket;
