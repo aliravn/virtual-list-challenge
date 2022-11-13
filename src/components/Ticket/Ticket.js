648eb16397fbef39400b6f78a3d2a58f70ddb37b
aliravn <alira.vn@gmail.com>
2022-11-13 14:43:52 +0100

    heighlight tickets priority with colors

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -8,7 +8,7 @@ const Ticket = ({ data }) => {
 	const { subject, priority, status, description } = data;
 	return (
 		<div className={classnames(styles.wrapper, styles.fontScaler)}>
-			<div className={styles.priority}>{priority}</div>
+			<div className={classnames(styles.priority, styles[priority])}>{priority}</div>
 			<div className={styles.subject}>{subject}</div>
 			<div className={styles.status}>{status}</div>
 			<div className={styles.description}>{description}</div>
