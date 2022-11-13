6143ae24714e1c7b139de96fe9e25bef63ee71d2
aliravn <alira.vn@gmail.com>
2022-11-13 13:51:48 +0100

    adapt ticket fonts and layout to look nicer on different screens

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import classnames from 'classnames';
 import styles from 'components/Ticket/Ticket.module.scss';
 
 const Ticket = ({ data }) => {
 	const { subject, priority, status, description } = data;
 	return (
-		<div className={styles.wrapper}>
+		<div className={classnames(styles.wrapper, styles.fontScaler)}>
 			<div className={styles.priority}>{priority}</div>
 			<div className={styles.subject}>{subject}</div>
 			<div className={styles.status}>{status}</div>
