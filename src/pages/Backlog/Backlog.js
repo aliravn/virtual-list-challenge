1ecbd4bde7eb832e1ec82ca4c9bea2989a5de19e
aliravn <alira.vn@gmail.com>
2022-11-13 15:23:42 +0100

    reduce list height and update number of items to generate

--- src/pages/Backlog/Backlog.js
+++ src/pages/Backlog/Backlog.js
@@ -4,18 +4,17 @@ import { createBacklog } from 'utils/dataFactory.js';
 import Ticket from 'components/Ticket/Ticket.js';
 import styles from 'pages/Backlog/Backlog.module.scss';
 
-const items = createBacklog(200);
+const items = createBacklog(12000);
 
 const Backlog = () => {
 	return (
 		<>
 			<div className={styles.container}>
 				<h1 className={styles.heading}>Ticket Backlog</h1>
-
 				<ScalableList
 					itemsTotal={items.length}
 					itemHeight={40}
-					listHeight={800}
+					listHeight={600}
 					renderItem={({ index, style }) => {
 						return (
 							<li key={index} style={style}>
