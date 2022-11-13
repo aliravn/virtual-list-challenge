4842b06d6457ea7af41ce555305df3aba6ba7199
aliravn <alira.vn@gmail.com>
2022-11-13 13:52:43 +0100

    adapt backlog width for large screens for better readability

--- src/pages/Backlog/Backlog.js
+++ src/pages/Backlog/Backlog.js
@@ -2,25 +2,29 @@ import React from 'react';
 import ScalableList from 'components/ScalableList/ScalableList.js';
 import { createBacklog } from 'utils/dataFactory.js';
 import Ticket from 'components/Ticket/Ticket.js';
+import styles from 'pages/Backlog/Backlog.module.scss';
 
 const items = createBacklog(200);
 
 const Backlog = () => {
 	return (
 		<>
-			<h1>Ticket Backlog</h1>
-			<ScalableList
-				itemsTotal={items.length}
-				itemHeight={40}
-				listHeight={800}
-				renderItem={({ index, style }) => {
-					return (
-						<li key={index} style={style}>
-							<Ticket data={items[index]} />
-						</li>
-					);
-				}}
-			/>
+			<div className={styles.container}>
+				<h1 className={styles.heading}>Ticket Backlog</h1>
+
+				<ScalableList
+					itemsTotal={items.length}
+					itemHeight={40}
+					listHeight={800}
+					renderItem={({ index, style }) => {
+						return (
+							<li key={index} style={style}>
+								<Ticket data={items[index]} />
+							</li>
+						);
+					}}
+				/>
+			</div>
 		</>
 	);
 };
